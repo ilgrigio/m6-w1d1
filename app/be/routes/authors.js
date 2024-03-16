b487a0e71933ea6c9411c1260d3ad62257f277dd
Andrea Sattin <andsattin@gmail.com>
2024-03-16 23:00:06 +0100

    patch request

--- app/be/routes/authors.js
+++ app/be/routes/authors.js
@@ -3,6 +3,7 @@ const router = express.Router();
 // Prendo dalla cartella models il modello utenti
 const AuthorsModel = require("../models/authors");
 
+// GET
 router.get("/getAuthors", async (request, response) => {
   try {
     const authors = await AuthorsModel.find(); // .find ritorno tutto quello che c'è nella collection 'users'
@@ -15,6 +16,7 @@ router.get("/getAuthors", async (request, response) => {
   }
 });
 
+// GET ID
 router.get("/getAuthor/:id", async (request, response) => {
   const { id } = request.params;
 
@@ -36,6 +38,7 @@ router.get("/getAuthor/:id", async (request, response) => {
   }
 });
 
+// POST
 router.post("/createAuthor", async (request, response) => {
   const newAuthor = new AuthorsModel({
     firstName: request.body.firstName,
@@ -51,6 +54,36 @@ router.post("/createAuthor", async (request, response) => {
       statusCode: 201,
       payload: authorToSave,
     });
+  } catch (error) {
+    response.status(500).send({
+      statusCode: 500,
+      message: "Internal server error",
+    });
+  }
+});
+
+// PATCH
+router.patch("/updateAuthor/:id", async (request, response) => {
+  const { id } = request.params;
+
+  try {
+    const author = await AuthorsModel.findById(id);
+    if (!author) {
+      response.status(404).send({
+        statusCode: 404,
+        message: "The requested author doesn't exist",
+      });
+    }
+
+    const updatedData = request.body;
+    // Quando il documento è stato aggiornato ci ritorni le ultime modifiche fatte
+    const options = { new: true }; // Ritorna il nuovo documento
+    const result = await AuthorsModel.findByIdAndUpdate(
+      id,
+      updatedData,
+      options
+    );
+    response.status(200).send(result);
   } catch (error) {
     response.status(500).send({
       statusCode: 500,
