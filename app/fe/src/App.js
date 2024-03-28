7535404bedbab7fc89492b54a76f46a8dba2ae5e
Andrea Sattin <andsattin@gmail.com>
2024-03-29 00:48:23 +0100

    signUpForm

--- app/fe/src/App.js
+++ app/fe/src/App.js
@@ -1,15 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Login from './pages/Login'
-import SignupForm from './components/signupForm/SignupForm'
 
 const App = () => {
     return (
         <Router>
             <Routes>
-                <Route exact path="/" element={<SignupForm />} />
                 <Route exact path="/" element={<Login />} />
-                <Route exact path="/home" element={<Home />} />
+                <Route path="/home" element={<Home />} />
             </Routes>
         </Router>
     )
--- app/fe/src/components/signupForm/SignupForm.jsx
+++ app/fe/src/components/signupForm/SignupForm.jsx
@@ -31,43 +31,63 @@ const SignupForm = () => {
     }
 
     return (
-        <form onSubmit={onSubmit}>
-            <input
-                onChange={onChangeInput}
-                type="text"
-                name="userName"
-                placeholder="Inserisci user"
-            />
-            {/* <input onChange={onChangeInput} type="text" name="userSurname" /> */}
-            <input
-                onChange={onChangeInput}
-                type="password"
-                name="password"
-                placeholder="Inserisci la password"
-            />
-            <input
-                onChange={onChangeInput}
-                type="email"
-                name="email"
-                placeholder="Inserisci email"
-            />
-            <input
-                onChange={onChangeInput}
-                type="text"
-                name="age"
-                placeholder="Inserisci l'età"
-            />
-            <input
-                onChange={onChangeInput}
-                type="text"
-                name="role"
-                placeholder="Inserisci il ruolo"
-            />
+        <div className="container">
+            <div className="row d-flex justify-content-center">
+                <div className="col-md-6">
+                    <h2 className="text-center text-dark mt-5">Registrati</h2>
 
-            <button className="btn btn-primary" type="submit">
-                Registrati
-            </button>
-        </form>
+                    <div className="card my-5">
+                        <form onSubmit={onSubmit}>
+                            <div className="mb-3">
+                                <input
+                                    onChange={onChangeInput}
+                                    type="text"
+                                    name="userName"
+                                    placeholder="Inserisci user"
+                                />
+                            </div>
+                            <div className="mb-3">
+                                <input
+                                    onChange={onChangeInput}
+                                    type="password"
+                                    name="password"
+                                    placeholder="Inserisci la password"
+                                />
+                            </div>
+                            <div className="mb-3">
+                                <input
+                                    onChange={onChangeInput}
+                                    type="email"
+                                    name="email"
+                                    placeholder="Inserisci email"
+                                />
+                            </div>
+                            <div className="mb-3">
+                                <input
+                                    onChange={onChangeInput}
+                                    type="age"
+                                    name="age"
+                                    placeholder="Inserisci l'età"
+                                />
+                            </div>
+                            <div className="mb-3">
+                                <input
+                                    onChange={onChangeInput}
+                                    type="text"
+                                    name="role"
+                                    placeholder="Inserisci il ruolo"
+                                />
+                            </div>
+                            <button className="btn btn-primary" type="submit">
+                                Registrati
+                            </button>
+
+                            {/* Sei già registrato? */}
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 export default SignupForm
